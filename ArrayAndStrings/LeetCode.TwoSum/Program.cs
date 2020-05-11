90a46da4b6c79f25ef98aa1f6c7bf9f7294d6961
brkzkn <burakozkan123@gmail.com>
2020-05-11 17:44:10 +0300

    - add new project

---
+++ ArrayAndStrings/LeetCode.TwoSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode.TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
