beff899d3497d4b79d0e30d0a6a890ea7483f793
brkzkn <burakozkan123@gmail.com>
2020-05-12 16:36:05 +0300

    - add Group Anagrams solution

--- ArrayAndStrings/LeetCode.GroupAnagrams/Program.cs
+++ ArrayAndStrings/LeetCode.GroupAnagrams/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace LeetCode.GroupAnagrams
+namespace LeetCode.TwoSum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var output = GroupAnagrams(input);
+
+            foreach (var item in output)
+            {
+                Console.WriteLine($"[{string.Join(",", item)}]");
+            }
+        }
+
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+            foreach (var str in strs)
+            {
+                char[] chr = str.ToCharArray();
+                Array.Sort(chr);
+                String sortedText = new String(chr);
+                if (map.TryGetValue(sortedText, out IList<string> anagramGroup))
+                    anagramGroup.Add(str);
+                else
+                    map.Add(sortedText, new List<string>() { str });
+            }
+            return map.Values.ToList();
         }
     }
 }
---
+++ ArrayAndStrings/LeetCode.GroupAnagrams/Readme.md
@@ -0,0 +1,28 @@
+﻿# Question: Group Anagrams
+
+Given an array of strings, group anagrams together.
+
+**Example:**
+
+```c#
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+```
+
+**Note:**
+
+- All inputs will be in lowercase.
+- The order of your output does not matter
+
+# LeetCode Info
+
+**Category:** Arrays and Strings
+
+**Company:** Amazon
+
+**Level:** Medium
