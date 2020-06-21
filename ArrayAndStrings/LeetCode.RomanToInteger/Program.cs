7274c904cc7a820bdc3ffb2247568963d0d9a6f8
brkzkn <burakozkan123@gmail.com>
2020-06-21 15:59:39 +0300

    - implement roman to integer

---
+++ ArrayAndStrings/LeetCode.RomanToInteger/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.RomanToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int RomanToInt(string roman)
+        {
+            Dictionary<char, int> romanInteger = new Dictionary<char, int>()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 },
+            };
+
+            int result = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i > 0 && romanInteger[roman[i]] > romanInteger[roman[i - 1]])
+                {
+                    result += romanInteger[roman[i]] - 2 * romanInteger[roman[i - 1]];
+                }
+                else
+                {
+                    result += romanInteger[roman[i]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
