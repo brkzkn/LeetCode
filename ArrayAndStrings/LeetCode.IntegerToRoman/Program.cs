6931d39475ee8de48282632ea2da0e9f697704d1
brkzkn <burakozkan123@gmail.com>
2020-06-21 15:43:27 +0300

    - implement IntToRoman question

---
+++ ArrayAndStrings/LeetCode.IntegerToRoman/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeetCode.IntegerToRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1994;
+            Console.WriteLine($"Number: {number} is equal to Roman: {IntToRoman(number)}");
+        }
+
+        static string IntToRoman(int num)
+        {
+            string[] thousands = new string[] { "", "M", "MM", "MMM" };
+            string[] houndreds = new string[] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] tens = new string[] { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] units = new string[] { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            return thousands[num / 1000] +
+                houndreds[(num % 1000) / 100] +
+                tens[(num % 100) / 10] +
+                units[num % 10];
+        }
+    }
+}
